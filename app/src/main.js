8e9e78b8bd9fd7ca1ff17a3005bc35ed33213d96
Anthony Bui <anthonybui@gmail.com>
2017-11-28 02:01:28 -0500

    WEBSOCKET_URL env

--- app/src/main.js
+++ app/src/main.js
@@ -5,7 +5,8 @@ console.info('Tada! It works. Now build something awesome.');
 // TODO: WTF WHY IS THIS BROKEN???
 // const WebSocket = require('ws');
 
-const ws = new WebSocket(process.env.WEBSOCKET);
+console.log(process.env.WEBSOCKET_URL);
+const ws = new WebSocket(process.env.WEBSOCKET_URL);
 
 ws.onopen = function () {
   console.log('connection opened');
