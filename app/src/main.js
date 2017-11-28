82a951b03cee4584a63fd6437303fd2265299be5
Anthony Bui <anthonybui@gmail.com>
2017-11-28 03:00:29 -0500

    Dynamic colors from websockets / window.rotaryValue!

--- app/src/main.js
+++ app/src/main.js
@@ -4,13 +4,3 @@ console.info('Tada! It works. Now build something awesome.');
 
 // TODO: WTF WHY IS THIS BROKEN???
 // const WebSocket = require('ws');
-
-const ws = new WebSocket("wss://radiant-dusk-69055.herokuapp.com");
-
-ws.onopen = function () {
-  console.log('connection opened');
-};
-
-ws.onmessage = function (evt) {
-  console.log(evt.data);
-};
