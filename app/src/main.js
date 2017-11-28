9ddfa793f9ff0ddd8d349ad2a7d226a64d6e1ab5
Anthony Bui <anthonybui@gmail.com>
2017-11-28 02:02:46 -0500

    Just fucking hardcode it

--- app/src/main.js
+++ app/src/main.js
@@ -5,8 +5,7 @@ console.info('Tada! It works. Now build something awesome.');
 // TODO: WTF WHY IS THIS BROKEN???
 // const WebSocket = require('ws');
 
-console.log(process.env.WEBSOCKET_URL);
-const ws = new WebSocket(process.env.WEBSOCKET_URL);
+const ws = new WebSocket("ws://radiant-dusk-69055.herokuapp.com");
 
 ws.onopen = function () {
   console.log('connection opened');
