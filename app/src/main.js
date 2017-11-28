69cc925f131c284237da27584c49af830fecc8be
Anthony Bui <anthonybui@gmail.com>
2017-11-28 01:55:49 -0500

    websockets
    * broadcast on server
    * log on client

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,16 @@
 import './styles/base.scss';
 
 console.info('Tada! It works. Now build something awesome.');
+
+// TODO: WTF WHY IS THIS BROKEN???
+// const WebSocket = require('ws');
+
+const ws = new WebSocket(process.env.WEBSOCKET);
+
+ws.onopen = function () {
+  console.log('connection opened');
+};
+
+ws.onmessage = function (evt) {
+  console.log(evt.data);
+};
--- server/index.js
+++ server/index.js
@@ -3,11 +3,38 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const http = require('http');
 const config = require('../config/webpack.config.js');
+const url = require('url');
+
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
+
+const WebSocket = require('ws');
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
 
 if (isDeveloping) {
   const compiler = webpack(config);
@@ -37,11 +64,7 @@ if (isDeveloping) {
   });
 }
 
-app.listen(port, (err) => {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-  }
-  // eslint-disable-next-line no-console
-  console.info(`Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+
+server.listen(port, function listening() {
+  console.log('Listening on %d', server.address().port);
 });
