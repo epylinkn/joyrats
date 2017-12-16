53eab4293046a35a3af99e964d1241c632d2690a
Anthony Bui <anthonybui@gmail.com>
2017-12-16 15:06:25 -0500

    Update webpack-production.config.js

--- config/webpack-production.config.js
+++ config/webpack-production.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 const devConfig = require('./webpack.config.js');
 
 const config = Object.assign({}, devConfig, {
-  entry: [
-    path.join(__dirname, '/../app/src/main.js'),
-  ],
+  entry: {
+    main: path.join(__dirname, '../app/src/main.js'),
+    arkanoid: path.join(__dirname, '../app/src/arkanoid.js'),
+    knob: path.join(__dirname, '../app/src/knob.js'),
+  },
 });
 
 module.exports = config;
